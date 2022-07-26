cec89cb46cc67d709ace756afb633924ba596868
Rei <109879740+ArtorianaRei@users.noreply.github.com>
2022-07-26 15:07:08 +1000

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ Translator              = 'google'
 # Translator            | 'google' or 'deepl'
 
 GoogleTranslate_suffix  = 'com'
-lang_Home               = 'en'
-lang_Away               = 'ja'
+Lang_Home               = 'en'
+Lang_Away               = 'ja'
 Lang_Ignore             = ["af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "ma", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu"]
 Ignore_Users            = ['Streamelements', 'SoundAlerts']
 Ignore_Line             = ['http', 'com', 'www', 'yuurei2', 'yuureiri', ':']
