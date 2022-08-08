33cbcab02c98446d54538d1c7ae1356b1cd80ba7
Rei <109879740+ArtorianaRei@users.noreply.github.com>
2022-08-09 08:58:43 +1000

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,7 @@ TargetLangs             = ['ja']
 ReadOnlyTheseLang       = ['ja']
 Lang_Ignore             = ["af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "ma", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu"]
 Ignore_Users            = ['Streamelements', 'SoundAlerts']
-Ignore_Line             = ['http', 'com', 'www',  'yuureiri']
-Delete_Words            = [':', ':-(', ':-)', ':-/', ':-D', ':-O', ':-P', ':-Z', ':-o', ':-p', ':-z', ':-|', ':/', ':D', ':O', ':P', ':Z', ':o', ':p', ':z', ':|', ':3', ':)', ':>', ':(', ':-(', 'riri', 'yuurei2Hugs', 'yuurei2Cheer', 'yuurei2Angy', 'yuurei2Bonk', 'yuurei2Cozy', 'yuurei2Heh', 'yuurei2Itsfine', 'yuurei2Kanpai', 'yuurei2Karaoke', 'yuurei2Naya', 'yuurei2Ojousama', 'yuurei2Rip', 'yuurei2Scream', 'yuurei2Shrimp', 'yuurei2Shy', 'yuurei2Sweat', 'yuurei2Tea', 'yuurei2Wow', 'yuurei2Yuki', 'yozora']
-
+Ignore_Line             = ['http', 'com', 'www']
+Delete_Words            = [':']
 Show_ByName             = True
 Debug                   = False
