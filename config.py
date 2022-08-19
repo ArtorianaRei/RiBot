7df541ac1b8c9c5be5c5c44963964cfad3f372b2
Rei <109879740+ArtorianaRei@users.noreply.github.com>
2022-08-19 13:32:39 +1000

    Update config.py
    moved bot prefix to config

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ Twitch_Channel          = 'INPUT TWITCH CHANNEL'
 Twitch_Nick             = 'INPUT BOT USERNAME'
 Twitch_OAUTH            = 'INPUT OAUTH KEY'                                   ## https://twitchtokengenerator.com/
 DeepL_AUTH              = ''
+Bot_Prefix              = "!!"                                                  ## i.e. "!" or "?"
 Bot_Color               = 'DodgerBlue' 
 ## Alternate Colors      |  'Blue', 'Coral', 'DodgerBlue', 'SpringGreen', 'YellowGreen', 'Green', 'OrangeRed', 'Red', 'GoldenRod', 'HotPink', 'CadetBlue', 'SeaGreen', 'Chocolate', 'BlueViolet', 'Firebrick'
 
-Translator              = 'google'
+Translator              = 'deepl'
 ## Translator            | 'google' or 'deepl'
 
 GoogleTranslate_suffix  = 'com'
@@ -13,7 +14,7 @@ Lang_Home               = 'en'
 Lang_Away               = 'ja'
 TargetLangs             = ['ja']
 ReadOnlyTheseLang       = ['ja']
-Lang_Ignore             = ["af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "ma", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu"]
+Lang_Ignore             = ["af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "jw", "ko", "kn", "kk", "km", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "ma", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu"]
 Ignore_Users            = ['Streamelements', 'SoundAlerts']
 Ignore_Line             = ['http', 'com', 'www']
 Delete_Words            = [':']
